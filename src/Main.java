536acca11e9559922c4af8098add88914fdcb8c1
ZachiBaharav <zachi@baharav.org>
2017-09-20 08:13:49 -0700

    Added answers.

--- src/Main.java
+++ src/Main.java
@@ -33,16 +33,29 @@ public class Main {
         // Homework from here on.
         System.out.println("---\nHomework from here on\n----");
         System.out.println("0x" + str + " as a Dec: " + BaseConverter.Hex2Dec(str));
+        System.out.println("--ANSWER--");
+        System.out.println("0x10100111 as a Dec: 167");
+        System.out.println("\n\n");
 
-        
         num = 20 ;
         str = BaseConverter.Dec2Oct(num);
         System.out.println(num + " as Octal: 0" + str);
+        System.out.println("--ANSWER--");
+        System.out.println("20 as Octal: 024");
+        System.out.println("\n\n");
+
+
         System.out.println("0" + str + " as a Decimal: " + BaseConverter.Oct2Dec(str));
+        System.out.println("--ANSWER--");
+        System.out.println("024 as a Decimal: 20");
+        System.out.println("\n\n");
 
         
         str = BaseConverter.Dec2Hex(num);
         System.out.println("0x" + str + " as a Binary: " + BaseConverter.Hex2Bin(str));
+        System.out.println("--ANSWER--");
+        System.out.println("0x14 as a Binary: 10100");
+        System.out.println("\n\n");
         
     }
 }
